b490f61eebf5641f43c0c03968955443b4f8f1cb
veerdo00 <david.vandeveerdonk@pinkroccade.nl>
2020-01-07 16:55:39 +0100

    added command detection

--- discoBot.js
+++ discoBot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.io');
 const log = require('winston');
 const auth = require('./auth.json');
 
+
 //Logging stuff
 log.remove(log.transports.Console);
 log.add(new log.transports.Console, {
@@ -17,6 +18,8 @@ const bot = new Discord.Client({
 
 //When bot is ready log this
 bot.on('ready', function(evt){
+    var offTopic = bot.channels.get("664085438562304002");
+    console.log(offTopic);
     log.info('Connected');
     log.info('Logged in as: ');
     log.info(bot.username + ' - (' + bot.id + ')');
@@ -24,19 +27,41 @@ bot.on('ready', function(evt){
 
 const ikBenRe = /ik ben (.*)/;
 
-bot.on('message', function(user, userID, channelID, message, evt){
 
+bot.on('message', function(user, userID, channelID, message, evt){
+    console.log(message);
+    if(message.startsWith('!')){
+        let command = message.substring(1);
+        log.info('command triggered ' + command);
+        switch (command) {
+            case 'lokroep':
+                console.log(message.member);
+                const andereChannel = message.member.channels.find("name", "Die andere");
+                user.setVoiceChannel(andereChannel)
+                .then(() => console.log(`Moved ${member.displayName}`))
+                .catch(console.error);
+                break;
+            default:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Command not recognized'
+                })
+                break;
+        } 
+    }else{
+
+        let results = ikBenRe.exec(message);
     
-    let results = ikBenRe.exec(message);
-    
-    if(results){
-        if(results[1] != 'DadBot!'){
-            let name = results[1];
-            let msg = 'Hallo ' + results[1] + ', ik ben DadBot!';
-            bot.sendMessage({
-                to: channelID,
-                message: msg
-            });
+        if(results){
+            if(results[1] != 'DadBot!'){
+                let name = results[1];
+                let msg = 'Hallo ' + results[1] + ', ik ben DiscoBot!';
+                bot.sendMessage({
+                    to: channelID,
+                    message: msg
+                });
+            }
         }
     }
 });
+
